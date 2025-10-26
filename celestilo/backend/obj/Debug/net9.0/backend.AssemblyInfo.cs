a815f85a28bf650f88c21be7a2ae0918e2bb9645
0CrazyLove <axelvasquez582@gmail.com>
2025-10-26 16:02:52 -0500

    ...

--- celestilo/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ celestilo/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e619eb3a303758b807cfdf95a006472e34dfef5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31f5d3a2e34b8b81dcb142578974c36a27f9c02")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
