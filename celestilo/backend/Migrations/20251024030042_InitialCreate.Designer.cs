2a0e6c13f67484d6744119300de3ea69ec018448
0CrazyLove <axelvasquez582@gmail.com>
2025-10-31 16:06:22 -0500

    refactor(migrations): add namespace to migration files

--- celestilo/backend/Migrations/20251024030042_InitialCreate.Designer.cs
+++ celestilo/backend/Migrations/20251024030042_InitialCreate.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-
+using backend.Data;
 #nullable disable
 
 namespace backend.Migrations
